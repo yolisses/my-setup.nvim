6b0ae03fb24edd56adb8ba495b39be345f3567e5
Yolisses <iamurise@gmail.com>
2026-08-06 15:07:29 -0300

    Set formatter for react files

--- init.lua
+++ init.lua
@@ -840,8 +840,10 @@ do
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
       javascript = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
       svelte = { 'prettierd' },
       typescript = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
       html = { 'prettierd' },
       css = { 'prettierd' },
       markdown = { 'prettierd' },
